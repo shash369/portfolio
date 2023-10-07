eb32f26e3a3c099bd9fda38bb53e76cdfd0ce45d
shashwat shukla <144621162+shash369@users.noreply.github.com>
2023-10-07 14:20:28 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,12 @@
+Shashwat Shukla 
+shashwat2211077@akgec.ac.in
+shuklashashwat009@gmail.com
+soiety::OSS(Open Source Softtware)
+branch::CSIT
+section::CSIT-2
+old section ::s-19
+college-name::ajay kumar garg engineering college
+ghaziyabad UTTAR PRADESH
+INDIA(+91)
+
+
---
+++ task1.js
@@ -0,0 +1,15 @@
+var typed=new Typed(".tech" , {
+    strings:["Front End Dev","Back End Dev","FULL STACK"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true,
+});
+
+var typed=new Typed(".intrest" , {
+    strings:["also I am really intrested in football,chess and vollyball beacuse Each one provides a unique flavor of excitement, strategy, and teamwork, making every day an adventure filled with thrilling moments."],
+    typeSpeed:50,
+    backSpeed:55,
+    backDelay:1000,
+    loop:Infinity,
+});
